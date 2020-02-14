9c48f99ec1163f30ab0565cae3e11f763459e7b8
mkaulfers <matthewkaulfers@student.fullsail.edu>
2020-02-14 13:45:05 -0600

    REFACTOR: Refactored the storyboards to have their own space for usability.

---
+++ GrandmasCookbook/GrandmasCookbook/FeaturedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeaturedViewController.swift
+//  GrandmasCookbook
+//
+//  Created by Matthew Kaulfers on 2/14/20.
+//  Copyright © 2020 Matthew Kaulfers. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
