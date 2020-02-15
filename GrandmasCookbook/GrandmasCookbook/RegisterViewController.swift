a40d52f421fb0cd17c27ab694381dbbe76c9fd44
mkaulfers <matthewkaulfers@student.fullsail.edu>
2020-02-15 16:50:33 -0600

    ADD: Added firestore and prepped method to store the user profile.

--- GrandmasCookbook/GrandmasCookbook/RegisterViewController.swift
+++ GrandmasCookbook/GrandmasCookbook/RegisterViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     //MARK: - InputFields
     
     @IBOutlet weak var errorLabel: UILabel!
@@ -21,17 +22,8 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setUpElements()
-    }
-    
-    //MARK: - Custom Methods
-    
-    func setUpElements()
-    {
-        //INFO: Hide the error text field, no errors yet.
-        errorLabel.alpha = 0
         
+        setUpElements()
     }
     
     //MARK: - Button Actions
@@ -40,9 +32,102 @@ class RegisterViewController: UIViewController {
     {
         dismiss(animated: true, completion: nil)
     }
-
+    
     @IBAction func registerButtonSelected(_ sender: Any)
     {
+        //Validate the fields
+        let error = validateFields()
+        
+        if error != nil
+        {
+            //INFO: Show error.
+            showError(error!)
+        }
+        else
+        {
+            //Create the user
+            Auth.auth().createUser(withEmail: <#T##String#>, password: <#T##String#>) { (result, err) in
+                
+                //INFO: Check for errors
+                if err != nil {
+                    //INFO: There was an error.
+                    self.showError("Error creating user")
+                }
+                else{
+                    //INFO: User was created successfully.
+                    //TODO: - STOPPED HERE ADDING FIRESTORE (USER)
+                }
+            }
+            
+            //Transition to profile page
+        }
+    }
+    
+    //MARK: - Custom Methods
+    
+    func showError(_ message: String)
+    {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
+    func setUpElements()
+    {
+        //INFO: Hide the error text field, no errors yet.
+        errorLabel.alpha = 0
         
     }
+    
+    //MARK: Field Validations
+    //Checks the fields to ensure that the data is correct. If it is then return nil, otherwise return error message.
+    func validateFields() -> String?
+    {
+        //INFO: Ensure that the fields are not blank.
+        if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            confirmPasswordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+        {
+            return "Please fill in all fields."
+        }
+        
+        //INFO: Make sure that the email is valid.
+        if !isEmailValid(emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines))
+        {
+            return "Please enter a valid email."
+        }
+        
+        //INFO: Make sure passwords match
+        if passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines) != confirmPasswordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        {
+            return "Your passwords don't match."
+        }
+        
+        
+        //INFO: Make sure that the passwords are secure.
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if !isPasswordValid(cleanedPassword)
+        {
+            return "Please make sure your password is at least 8 characters, contains a special character and a number."
+        }
+        
+        return nil
+    }
+    
+    func isEmailValid(_ email: String) -> Bool
+    {
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", "\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\b")
+        return emailTest.evaluate(with: email)
+    }
+    
+    func isPasswordValid(_ password: String) -> Bool
+    {
+        //INFO: Check password against Regular Expression. 8 Char min, Special Char, Number
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+    
 }
