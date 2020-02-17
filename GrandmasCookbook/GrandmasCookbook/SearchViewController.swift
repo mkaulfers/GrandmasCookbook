9ab6dd15b7c8a6efc199daf039f8bf00064b8a2f
mkaulfers <matthewkaulfers@student.fullsail.edu>
2020-02-16 18:41:49 -0600

    CHANGE: Changed the gradient UIImages to a solid color. Replaced UIImageViews with UIViews, this was done through the XML in the event of undefined error.

--- GrandmasCookbook/GrandmasCookbook/SearchViewController.swift
+++ GrandmasCookbook/GrandmasCookbook/SearchViewController.swift
@@ -8,23 +8,50 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
+    //MARK: - Class Vars and Outlets
+    @IBOutlet var recipesTableView: UITableView!
+    
+    //MARK: - Parent View Controller Management
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //INFO: Load the header view.
+        let headerNib = UINib.init(nibName: "searchHeader", bundle: Bundle.main)
+        recipesTableView.register(headerNib, forHeaderFooterViewReuseIdentifier: "searchHeader")
+        
+        //INFO: Hide the separator
+        recipesTableView.separatorColor = .clear
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Table View Managment
+    //INFO: Anytime a search is updated do this check.
+    func updateSearchResults(for searchController: UISearchController) {
+        //TODO: Update what the search results do.
     }
-    */
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let headerView = recipesTableView.dequeueReusableHeaderFooterView(withIdentifier: "searchHeader") as! searchHeader
+        return headerView.bounds.size.height
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        if section == 0
+        {
+            return recipesTableView.dequeueReusableHeaderFooterView(withIdentifier: "searchHeader") as! searchHeader
+        }
+        
+        //TODO: Temporary placeholder.
+        return UIView()
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
---
+++ GrandmasCookbook/GrandmasCookbook/searchHeader.swift
@@ -0,0 +1,13 @@
+//
+//  searchHeader.swift
+//  GrandmasCookbook
+//
+//  Created by Matthew Kaulfers on 2/16/20.
+//  Copyright © 2020 Matthew Kaulfers. All rights reserved.
+//
+
+import UIKit
+
+class searchHeader: UITableViewHeaderFooterView {
+    @IBOutlet var searchBarAndScope: UISearchBar! 
+}
